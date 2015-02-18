eddd821cb4814a105e42250bce9caa585b087610
dmiro <lite.3engine@gmail.com>
2015-02-19 00:29:01 +0100

    delete method

--- bow.py
+++ bow.py
@@ -2,23 +2,50 @@
 __author__ = 'dmiro'
 import copy
 
-
 class BagOfWords(object):
 
     def __init__(self):
         self._bow = {}
 
-    def add(self, values):
+    def _calc(self, operation, words):
+        if isinstance(words, basestring):
+            words = [words]
+        for word in words:
+            if word in self._bow:
+                self._bow[word] = operation(self._bow[word])
+            else:
+                self._bow[word] = operation(0)
+            if self._bow[word] < 1:
+                del self._bow[word]
+
+##    http://stackoverflow.com/questions/10664856/make-dictionary-with-duplicate-keys-in-python
+##    def _calc2(self, operation, other):
+##        if isinstance(other, BagOfWords):
+##            result = copy.deepcopy (self) 
+##            for word, value in other._bow.items():
+##                if word in result._bow:
+##                    result._bow[word] = operation(self._bow[word], value)
+##                else:
+##                    result._bow[word] = operation(0, value)
+##                if result._bow[word] < 1:
+##                    del result._bow[word]
+##            return result
+##        else:
+##            result = copy.deepcopy(self)
+##            result._calc(result, other)
+##            return result
+    
+    def add(self, words):
         """Add word or word list to bag of words
         :param values: word or word list to add
         :return:nothing"""
-        if isinstance(values, basestring):
-            values = [values]
-        for value in values:
-            if value in self._bow:
-                self._bow[value] += 1
-            else:
-                self._bow[value] = 1
+        self._calc(lambda x: x+1, words)
+
+    def delete(self, words):
+        """Del word or word list from bag of words
+        :param values: word or word list to add
+        :return:nothing"""
+        self._calc(lambda x: x-1, words)
 
     def freq(self, word):
         """Returning the frequency of a word
@@ -34,7 +61,7 @@ class BagOfWords(object):
         :param other: BagOfWords
         :return: BagOfWords"""
         if isinstance(other, BagOfWords):
-            result = copy.deepcopy(self) 
+            result = copy.deepcopy (self) 
             for key in other._bow:
                 if key in result._bow:
                     result._bow[key] += other._bow[key]
@@ -45,7 +72,7 @@ class BagOfWords(object):
             result = copy.deepcopy(self)
             result.add(other)
             return result
-        
+      
     def __radd__(self, other):
         return self.__add__(other)
 
@@ -81,9 +108,6 @@ class BagOfWords(object):
     def words(self):
         """Word list contained in the object"""
         return self._bow.keys()
-
-
-        
 
 
 
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class BagOfWordsTest(unittest.TestCase):
     def setUp(self):
         self.bow = bow.BagOfWords()
 
-    def test_one_word(self):
+    def test_add_one_word(self):
         self.bow.add('David')
         self.bow.add('David')
         self.bow.add('David')
@@ -17,7 +17,7 @@ class BagOfWordsTest(unittest.TestCase):
         self.assertEqual(self.bow.freq('David'), 3)
         self.assertEqual(dict(self.bow), {'David':3})
 
-    def test_two_words(self):
+    def test_add_two_words(self):
         self.bow.add(u'David')
         self.bow.add(u'David')
         self.bow.add(u'Álex')
@@ -27,6 +27,40 @@ class BagOfWordsTest(unittest.TestCase):
         self.assertEqual(self.bow.freq('David'), 2)
         self.assertEqual(dict(self.bow), {u'Álex':1, 'David':2})
 
+    def test_del_one_word(self):
+        self.bow.delete('David')
+        self.assertEqual(dict(self.bow), {})
+        self.bow.add('David')
+        self.bow.delete('David')
+        self.assertEqual(dict(self.bow), {})
+        self.bow.add('David')
+        self.bow.add('David')
+        self.bow.delete('David')
+        self.assertEqual(self.bow.words, ['David'])
+        self.assertEqual(len(self.bow), 1)
+        self.assertEqual(self.bow.num, 1)
+        self.assertEqual(self.bow.freq('David'), 1)
+        self.assertEqual(dict(self.bow), {'David':1})
+
+    def test_del_two_word(self):
+        self.bow.delete('David')
+        self.bow.delete(u'Álex')
+        self.assertEqual(dict(self.bow), {})
+        self.bow.add('David')
+        self.bow.add(u'Álex')
+        self.bow.delete('David')
+        self.bow.delete(u'Álex')
+        self.assertEqual(dict(self.bow), {})
+        self.bow.add('David')
+        self.bow.add('David')
+        self.bow.delete('David')
+        self.bow.add(u'Álex')
+        self.assertEqual(self.bow.words, [u'Álex', 'David'])
+        self.assertEqual(len(self.bow), 2)
+        self.assertEqual(self.bow.num, 2)
+        self.assertEqual(self.bow.freq('David'), 1)
+        self.assertEqual(dict(self.bow), {u'Álex':1, 'David':1})
+        
     def test_join(self):
         a = bow.BagOfWords()
         b = bow.BagOfWords()
@@ -36,6 +70,8 @@ class BagOfWordsTest(unittest.TestCase):
         c.add('plane')
         total = a + b + c
         self.assertEqual(dict(total), {'car': 1, 'chair': 1, 'eye': 1, 'chicken': 3, 'plane': 1, 'ugly': 1})
+        total.delete(['chicken', 'chicken','car'])
+        self.assertEqual(dict(total), {'chair': 1, 'eye': 1, 'chicken': 1, 'plane': 1, 'ugly': 1})
         total.clear()
         total = total + ['car', 'chair', 'chicken'] + ['chicken', 'chicken', 'eye']
         total = 'ugly' + total
