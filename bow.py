b372613bee050bbaa5d7dd740741d9bff0917742
dmiro <davidmirom@hotmail.com>
2015-03-05 15:09:38 +0100

    I added some help text

--- bow.py
+++ bow.py
@@ -11,6 +11,7 @@ from json import JSONEncoder, JSONDecoder
 
 
 class BagOfWords(object):
+    """Implementing a bag of words with their frequency of usages"""
 
     def __init__(self, *args):
         self._bow = {}
@@ -29,15 +30,17 @@ class BagOfWords(object):
                     del self._bow[word]
 
     def add(self, *args):
-        """Add set of word, word list or word dict to bag of words
+        """Add set of word, word list or word dict to bag of words.
         :param args: set of word or word list to add
-        :return:nothing"""
+        :return:nothing
+        """
         self.__calc(lambda x,y: x+y, *args)
 
     def delete(self, *args):
-        """Delete set of word, word list or word dict to bag of words
+        """Delete set of word, word list or word dict to bag of words.
         :param args: set of word or word list to add
-        :return:nothing"""
+        :return:nothing
+        """
         self.__calc(lambda x,y: x-y, *args)
 
     def rates(self):
@@ -49,18 +52,20 @@ class BagOfWords(object):
             return {}
 
     def freq(self, word):
-        """Frequency of a word
+        """Frequency of a word.
         :param word: word to query
-        :return: frequency"""
+        :return: frequency
+        """
         if word in self._bow:
             return self._bow[word]
         else:
             return 0
 
     def rate(self, word):
-        """Rate of a word
+        """Rate of a word.
         :param word: word to query
-        :return: rate"""
+        :return: rate
+        """
         total = float(self.num())
         if total:
             return self.freq(word)/total
@@ -68,9 +73,10 @@ class BagOfWords(object):
             return 0
 
     def __add__(self, other):
-        """ Overloading of "+" operator to join BagOfWord+BagOfWord, BagOfWords+str or BagOfWords+list
+        """ Overloading of "+" operator to join BagOfWord+BagOfWord, BagOfWords+str or BagOfWords+list.
         :param other: BagOfWords, str or list
-        :return: BagOfWords"""
+        :return: BagOfWords
+        """
         result = self.copy()
         if isinstance(other, BagOfWords):
             result.add(dict(other))
@@ -79,9 +85,10 @@ class BagOfWords(object):
         return result
 
     def __sub__(self, other):
-        """ Overloading of "-" operator to join BagOfWord+BagOfWord, BagOfWords+str or BagOfWords+list
+        """ Overloading of "-" operator to join BagOfWord+BagOfWord, BagOfWords+str or BagOfWords+list.
         :param other: BagOfWords, str or list
-        :return: BagOfWords"""
+        :return: BagOfWords
+        """
         result = self.copy()
         if isinstance(other, BagOfWords):
             result.delete(dict(other))
@@ -120,19 +127,19 @@ class BagOfWords(object):
         return copy.deepcopy(self)
 
     def clear(self):
-        """Clear word list"""
+        """Clear word list."""
         self._bow.clear()
 
     def iteritems(self):
-        """Return an iterator over the word dictionary’s (word, frequency) pairs"""
+        """Return an iterator over the word dictionary’s (word, frequency) pairs."""
         return self._bow.iteritems()
 
     def keys(self):
-        """Word list contained in the object"""
+        """Word list contained in the object."""
         return self._bow.keys()
 
     def words(self):
-        """Word list contained in the object"""
+        """Word list contained in the object."""
         return self.keys()
 
     def items(self):
@@ -142,7 +149,7 @@ class BagOfWords(object):
         return self._bow.values()
 
     def num(self):
-        """Total number of words"""
+        """Total number of words."""
         return sum(self._bow.values())
 
     def has_key(self, key):
@@ -157,26 +164,27 @@ class BagOfWords(object):
 
 
 class TextFilters(object):
+    """Filters for transforming a text"""
 
     @staticmethod
     def upper(text):
-        """Convert text to uppercase"""
+        """Convert text to uppercase."""
         return text.upper()
 
     @staticmethod
     def lower(text):
-        """Convert text to lowercase"""
+        """Convert text to lowercase."""
         return text.lower()
 
     @staticmethod
     def invalid_chars(text):
-        """Remove invalid chars from a text"""
+        """Remove invalid chars from a text."""
         INVALID_CHARS = u"/\¨º-~#@|¡!,·$%&()¿?'[^""`]+}{><;,:.=*^_"
         return ''.join([char for char in text if char not in INVALID_CHARS])
 
     @staticmethod
     def html_to_text(text):
-        """Conversion from HTML markup to plain text"""
+        """Conversion from HTML markup to plain text."""
         class _HTMLParser(HTMLParser):
 
             def __init__(self):
@@ -199,12 +207,14 @@ class TextFilters(object):
 
 
 class WordFilters(object):
+    """Filters for transforming a set of words"""
 
     @staticmethod
     def stemming(lang, stemming, words):
-        """Lemmatize text
+        """Lemmatize text.
         :param lang: lang text to lemmatize
-        :param stemming: number loops of lemmatizing"""
+        :param stemming: number loops of lemmatizing
+        """
         import Stemmer as stemmer
         try:
             stemmer = stemmer.Stemmer(lang)
@@ -216,8 +226,9 @@ class WordFilters(object):
 
     @staticmethod
     def stopwords(lang, words):
-        """Remove stop words from a text
-        :param lang: language text where remove empty words"""
+        """Remove stop words from a text.
+        :param lang: language text where remove empty words
+        """
         import stop_words
         try:
             stopwords = stop_words.get_stop_words(lang)
@@ -227,31 +238,37 @@ class WordFilters(object):
 
     @staticmethod
     def normalize(words):
-        """Normalize chars from a text"""
+        """Normalize chars from a text."""
         return [''.join((char for char in unicodedata.normalize('NFD', unicode(word)) if unicodedata.category(char) != 'Mn'))
                 for word in words]
 
 
 class Tokenizer(object):
+    """Allows to break a string into tokens (set of words). Optionally allows you to set
+    filters before (TextFilters) and after (WordFilters) breaking the string into tokens.
+    """
 
     def __init__(self):
         object.__init__(self)
 
     def before_tokenizer(self, textfilters, text):
-        """function to call before tokenizer text
+        """function to call before tokenizer text.
         :param textfilters: static class with helper methods to filter text
-        :param text: The text will be split"""
+        :param text: The text will be split
+        """
         return text
 
     def after_tokenizer(self, wordfilters, words):
-        """function to call after tokenizer text
+        """function to call after tokenizer text.
         :param wordfilters: static class with helper methods to filter words
-        :param words: split words"""
+        :param words: split words
+        """
         return words
 
     def tokenizer(self, text):
-        """tokenize a text
-        :param text: text to tokenizer"""
+        """tokenize a text.
+        :param text: text to tokenizer
+        """
         text = self.before_tokenizer(TextFilters, text)
         words = text.split()
         words = self.after_tokenizer(WordFilters, words)
@@ -262,6 +279,10 @@ class Tokenizer(object):
 
 
 class DocumentClass(Tokenizer):
+    """Implementing a bag of words collection where all the bags of words are the same
+    category. Retrieves the text of a file, folder, url or zip, and save or retrieve
+    the collection in json format.
+    """
 
     def __init__(self, category):
         Tokenizer.__init__(self)
@@ -270,10 +291,11 @@ class DocumentClass(Tokenizer):
         self._total = BagOfWords()
 
     def read_text(self, id, text):
-        """The text is stored in a BagOfWords identified by id
+        """The text is stored in a BagOfWords identified by Id.
         :param id: BagOfWord identifier
         :param text: text to add a BagOfWords
-        :return: BagOfWords"""
+        :return: BagOfWords
+        """
         words = self.tokenizer(text)
         if id in self._docs:
             self._total -= self._docs[id]
@@ -282,9 +304,10 @@ class DocumentClass(Tokenizer):
         return self._docs[id]
 
     def read_files(self, *filenames):
-        """The contents of each file or files is stored in a BagOfWord identified by the filename
+        """The contents of each file or files is stored in a BagOfWord identified by the filename.
         :param *filenames: filenames to add
-        :return: BagOfWord dict"""
+        :return: BagOfWord dict
+        """
         docs = {}
         for filename in filenames:
             text = open(filename, 'r').read()
@@ -293,9 +316,11 @@ class DocumentClass(Tokenizer):
         return docs
 
     def read_dir(self, path):
-        """The contents of each file o files of a directory is stored in a BagOfWord identified by the filename
+        """The contents of each file o files of a directory is stored in a BagOfWord
+        identified by the filename.
         :param path: directoy path to add files
-        :return: BagOfWord dict"""
+        :return: BagOfWord dict
+        """
         fn = []
         for (_, _, filenames) in os.walk(path):
             fn.extend([os.path.join(path,f) for f in filenames])
@@ -303,9 +328,10 @@ class DocumentClass(Tokenizer):
         return self.read_files(*fn)
 
     def read_urls(self, *urls):
-        """The contents of each url or urls is stored in a BagOfWord identified by the url
+        """The contents of each url or urls is stored in a BagOfWord identified by the url.
         :param *urls: urls to add
-        :return: BagOfWord dict"""
+        :return: BagOfWord dict
+        """
         docs = {}
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20140129 Firefox/24.0'}
         for url in urls:
@@ -316,9 +342,11 @@ class DocumentClass(Tokenizer):
         return docs
 
     def read_zips(self, *zipfilenames):
-        """The contents of each file o files of a zip file is stored in a BagOfWord identified by the filename
+        """The contents of each file o files of a zip file is stored in a BagOfWord
+        identified by the filename.
         :param *zipfilenames: zip files to add
-        :return: BagOfWord dict"""
+        :return: BagOfWord dict
+        """
         docs = {}
         for zipfilename in zipfilenames:
             input_zip = ZipFile(zipfilename)
@@ -342,8 +370,9 @@ class DocumentClass(Tokenizer):
         return self._category
 
     def to_json(self):
-        """Convert DocumentClass object to json string
-        :return: json"""
+        """Convert DocumentClass object to json string.
+        :return: json
+        """
         class _Encoder(JSONEncoder):
 
             def default(self, obj):
@@ -360,9 +389,10 @@ class DocumentClass(Tokenizer):
 
     @staticmethod
     def from_json(json_):
-        """Convert json string to DocumentClass object
+        """Convert json string to DocumentClass object.
         :param json_: json string
-        :return: DocumentClass object"""
+        :return: DocumentClass object
+        """
         class _Decoder(JSONDecoder):
 
             def __init__(self):
@@ -389,6 +419,9 @@ class DocumentClass(Tokenizer):
 
 
 class DefaultTokenizer(Tokenizer):
+    """Tokenizer subclass that implements the text filters 'lower' and 'invalid_chars'
+    and the word filters 'stopwords', 'stemming' and 'normalize'.
+    """
 
     def __init__(self, lang='english', stemming=1):
          Tokenizer.__init__(self)
@@ -406,7 +439,11 @@ class DefaultTokenizer(Tokenizer):
         words = wordfilters.normalize(words)
         return words
 
+
 class SimpleTokenizer(Tokenizer):
+    """Tokenizer subclass that implements the text filters 'lower' and 'invalid_chars'
+    and the word filter 'normalize'.
+    """
 
     def __init__(self):
          Tokenizer.__init__(self)
@@ -422,6 +459,7 @@ class SimpleTokenizer(Tokenizer):
 
 
 class DefaultDocumentClass(DocumentClass, DefaultTokenizer):
+    """DefaultTokenizer and DocumentClass subclass"""
 
     def __init__(self, category, lang='english', stemming=1):
         DocumentClass.__init__(self, category)
@@ -429,6 +467,7 @@ class DefaultDocumentClass(DocumentClass, DefaultTokenizer):
 
 
 class SimpleDocumentClass(DocumentClass, SimpleTokenizer):
+    """SimpleTokenizer and DocumentClass subclass"""
 
     def __init__(self, category):
         DocumentClass.__init__(self, category)
